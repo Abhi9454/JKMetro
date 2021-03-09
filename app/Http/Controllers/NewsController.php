e6a5912dc6b1b4c4a48cc7d4800d170015209dc8
abhi9454 <mishra.abhi8888@gmail.com>
2021-03-09 21:21:53 +0530

    #m

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class NewsController extends Controller
 {
