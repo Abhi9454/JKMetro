9d65157f71f41f6a3e4c58cade9f32220e93be4e
abhi9454 <mishra.abhi8888@gmail.com>
2021-03-09 21:09:36 +0530

    #m

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -1,11 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Models\User as Users;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
