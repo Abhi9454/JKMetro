c239b86023f7f104ec5d1108b545cd8b6e74faf7
abhi9454 <mishra.abhi8888@gmail.com>
2021-03-09 21:44:09 +0530

    #m

--- resources/views/Pages/sidebar.blade.php
+++ resources/views/Pages/sidebar.blade.php
@@ -21,7 +21,7 @@
         <!-- Sidebar Menu -->
 
         @if(strcmp(Session::get("value"),"admin")==0)
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="user-panel mt-3 ml-2 pb-3 mb-3 d-flex">
             <div class="info">
                 <a href="#" class="d-block">{{Session::get("email")}}</a>
             </div>
