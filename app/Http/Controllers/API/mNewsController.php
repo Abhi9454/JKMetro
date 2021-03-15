49d3ea1a8e4d7f7b6e7a8a6ea9d5d2e49003fb90
abhi9454 <mishra.abhi8888@gmail.com>
2021-03-16 01:28:52 +0530

    #m

--- app/Http/Controllers/API/mNewsController.php
+++ app/Http/Controllers/API/mNewsController.php
@@ -81,8 +81,98 @@ class mNewsController extends Controller
         }
     }
 
-    public function getCategoryArticle(Request $request){
-        $articles = DB::table('articles')->where('article_category',$request->category_id)->orderBy('article_added_on', 'desc')->get();
+    public function getJammu(){
+        $articles = DB::table('articles')->where('article_category',1)->orderBy('article_added_on', 'desc')->get();
+        if (count($articles) != 0) {
+            for($x = 0; $x < count($articles); $x++ ){
+                $date = date_format(\date_create($articles[$x]->article_added_on), 'd-m-Y');
+                $category = DB::table('category')->where('category_id',$articles[$x]->article_category)->get();
+                $articles[$x]->category_name = $category[0]->category_name;
+                $articles[$x]->article_time = $date;
+            }
+            return response()->json(['success' => $articles], 200);
+        } else {
+            return response()->json(['error' => 'No Articles found'], 401);
+        }
+    }
+
+    public function getNational(){
+        $articles = DB::table('articles')->where('article_category',3)->orderBy('article_added_on', 'desc')->get();
+        if (count($articles) != 0) {
+            for($x = 0; $x < count($articles); $x++ ){
+                $date = date_format(\date_create($articles[$x]->article_added_on), 'd-m-Y');
+                $category = DB::table('category')->where('category_id',$articles[$x]->article_category)->get();
+                $articles[$x]->category_name = $category[0]->category_name;
+                $articles[$x]->article_time = $date;
+            }
+            return response()->json(['success' => $articles], 200);
+        } else {
+            return response()->json(['error' => 'No Articles found'], 401);
+        }
+    }
+
+    public function getInternational(){
+        $articles = DB::table('articles')->where('article_category',2)->orderBy('article_added_on', 'desc')->get();
+        if (count($articles) != 0) {
+            for($x = 0; $x < count($articles); $x++ ){
+                $date = date_format(\date_create($articles[$x]->article_added_on), 'd-m-Y');
+                $category = DB::table('category')->where('category_id',$articles[$x]->article_category)->get();
+                $articles[$x]->category_name = $category[0]->category_name;
+                $articles[$x]->article_time = $date;
+            }
+            return response()->json(['success' => $articles], 200);
+        } else {
+            return response()->json(['error' => 'No Articles found'], 401);
+        }
+    }
+
+    public function getSports(){
+        $articles = DB::table('articles')->where('article_category',4)->orderBy('article_added_on', 'desc')->get();
+        if (count($articles) != 0) {
+            for($x = 0; $x < count($articles); $x++ ){
+                $date = date_format(\date_create($articles[$x]->article_added_on), 'd-m-Y');
+                $category = DB::table('category')->where('category_id',$articles[$x]->article_category)->get();
+                $articles[$x]->category_name = $category[0]->category_name;
+                $articles[$x]->article_time = $date;
+            }
+            return response()->json(['success' => $articles], 200);
+        } else {
+            return response()->json(['error' => 'No Articles found'], 401);
+        }
+    }
+
+    public function getEditorial(){
+        $articles = DB::table('articles')->where('article_category',7)->orderBy('article_added_on', 'desc')->get();
+        if (count($articles) != 0) {
+            for($x = 0; $x < count($articles); $x++ ){
+                $date = date_format(\date_create($articles[$x]->article_added_on), 'd-m-Y');
+                $category = DB::table('category')->where('category_id',$articles[$x]->article_category)->get();
+                $articles[$x]->category_name = $category[0]->category_name;
+                $articles[$x]->article_time = $date;
+            }
+            return response()->json(['success' => $articles], 200);
+        } else {
+            return response()->json(['error' => 'No Articles found'], 401);
+        }
+    }
+
+    public function getEntertainment(){
+        $articles = DB::table('articles')->where('article_category', 6)->orderBy('article_added_on', 'desc')->get();
+        if (count($articles) != 0) {
+            for($x = 0; $x < count($articles); $x++ ){
+                $date = date_format(\date_create($articles[$x]->article_added_on), 'd-m-Y');
+                $category = DB::table('category')->where('category_id',$articles[$x]->article_category)->get();
+                $articles[$x]->category_name = $category[0]->category_name;
+                $articles[$x]->article_time = $date;
+            }
+            return response()->json(['success' => $articles], 200);
+        } else {
+            return response()->json(['error' => 'No Articles found'], 401);
+        }
+    }
+
+    public function getSpecial(){
+        $articles = DB::table('articles')->where('article_category',5)->orderBy('article_added_on', 'desc')->get();
         if (count($articles) != 0) {
             for($x = 0; $x < count($articles); $x++ ){
                 $date = date_format(\date_create($articles[$x]->article_added_on), 'd-m-Y');
