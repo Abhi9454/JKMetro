e83412507baeba88ca2b879fa9902286e1b69af5
abhi9454 <mishra.abhi8888@gmail.com>
2021-03-15 20:46:05 +0530

    #m

--- app/Http/Controllers/API/mNewsController.php
+++ app/Http/Controllers/API/mNewsController.php
@@ -15,7 +15,7 @@ class mNewsController extends Controller
     {
         $temp = DB::table('articles')->orderBy('article_added_on', 'desc')->take(1)->get();
         $data[] = $temp[0]->article_id;
-        $articles = DB::table('articles')->whereNotIn('article_id',$data)->whereNotIn('article_category_id',[4])->orderBy('article_added_on', 'desc')->get();
+        $articles = DB::table('articles')->whereNotIn('article_id',$data)->whereNotIn('article_category',[4])->orderBy('article_added_on', 'desc')->get();
         if (count($articles) != 0) {
             for($x = 0; $x < count($articles); $x++ ){
                 $date = date_format(\date_create($articles[$x]->article_added_on), 'd-m-Y');
